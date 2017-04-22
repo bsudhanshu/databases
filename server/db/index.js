90c2fd74244d8cb0eb533e2de50d7d30e22086f4
Hack Reactor Student <student@hackreactor.com>
2017-04-21 21:34:12 -0700

    Completed 1st test

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,16 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+// // mysql -u root -p;
+// var dbConnection;
 
+// dbConnection = mysql.createConnection({
+//   user: 'root',
+//   password: 'plantlife',
+//   database: 'chat'
+// });
+
+// dbConnection.connect();
+
+// var queryString = 'SELECT * FROM messages';
+// dbConnection.query(queryString);
--- server/schema.sql
+++ server/schema.sql
@@ -1,32 +1,36 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-DROP TABLE IF EXISTS 'messages';
-DROP TABLE IF EXISTS 'username';
-DROP TABLE IF EXISTS 'room';
-
-CREATE TABLE messages (
-  id int NOT NULL, 
-  message_text NULL varchar(255),
-  created_at NOT NULL date, 
-  FOREIGN KEY (id_user) REFERENCES username(id),
-  FOREIGN KEY (id_room) REFERENCES room(id),
-  PRIMARY KEY (id)
-);
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS username;
+DROP TABLE IF EXISTS room;
 
 CREATE TABLE username (
-  id int NOT NULL, 
-  name varchar(255),
+  id INT NOT NULL, 
+  name VARCHAR(255),
   PRIMARY KEY (id)  
 );
 
 CREATE TABLE room (
-  id int NOT NULL, 
-  room_name varchar(255),
+  id INT NOT NULL, 
+  room_name VARCHAR(255),
   PRIMARY KEY (id)
 );
 
+CREATE TABLE messages (
+  id INT NOT NULL, 
+  message_text VARCHAR(255) NULL,
+  -- created_at DATE NOT NULL, 
+  -- id_user INT NOT NULL,
+  -- id_room INT NOT NULL,
+  -- FOREIGN KEY (id_user) REFERENCES username(id),
+  -- FOREIGN KEY (id_room) REFERENCES room(id),
+  PRIMARY KEY (id)
+);
+
+-- INSERT INTO username VALUES (1, 'alan');
+-- INSERT INTO messages VALUES (1,"In mercy's name, three days is all I need.")
 
 
 /* Create other tables and define schemas for them here! */
