5c2ceb4bb64508d1495c651ab3677371f0a14ffd
Hack Reactor Student <student@hackreactor.com>
2017-04-21 17:04:22 -0700

    Completed schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,32 @@ CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS 'messages';
+DROP TABLE IF EXISTS 'username';
+DROP TABLE IF EXISTS 'room';
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int NOT NULL, 
+  message_text NULL varchar(255),
+  created_at NOT NULL date, 
+  FOREIGN KEY (id_user) REFERENCES username(id),
+  FOREIGN KEY (id_room) REFERENCES room(id),
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE username (
+  id int NOT NULL, 
+  name varchar(255),
+  PRIMARY KEY (id)  
+);
+
+CREATE TABLE room (
+  id int NOT NULL, 
+  room_name varchar(255),
+  PRIMARY KEY (id)
+);
+
+
 
 /* Create other tables and define schemas for them here! */
 
